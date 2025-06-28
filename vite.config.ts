f22a5498389d012c01dd197a302769c388fff41f
Nagarjun E <35624814+nagarjune@users.noreply.github.com>
2025-06-28 15:11:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/stump-and-summary-app/' : '/',
+  base: mode === 'production' ? '/CricScores/' : '/',
   server: {
     host: "::",
     port: 8080,
